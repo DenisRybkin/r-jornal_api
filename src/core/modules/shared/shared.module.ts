74a0c759cea15b5c3840fa361d53a2f6c9ba5293
DenisRybkin <denis.rybkin.94@mail.ru>
2023-01-10 23:20:36 +0300

    implementation shared module

---
+++ src/core/modules/shared/shared.module.ts
@@ -0,0 +1,29 @@
+import { HttpModule } from '@nestjs/axios';
+import { DynamicModule } from '@nestjs/common';
+import { ApiConfigService } from './services/api-config.service';
+import { DateService } from './services/date.service';
+import { GeneratorService } from './services/generator.service';
+import { UrlService } from './services/url.service';
+
+interface SharedModuleOptions {
+  isGlobal?: boolean;
+}
+
+export class SharedModule {
+  static forRoot(options?: SharedModuleOptions): DynamicModule {
+    const providers = [
+      ApiConfigService,
+      DateService,
+      GeneratorService,
+      UrlService,
+    ];
+
+    return {
+      module: SharedModule,
+      global: !!options?.isGlobal,
+      providers,
+      imports: [HttpModule],
+      exports: [...providers, HttpModule],
+    };
+  }
+}
