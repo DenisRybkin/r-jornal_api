6bcb35f0fa5c66400114a455a2e0ac2faf2067aa
DenisRybkin <denis.rybkin.94@mail.ru>
2023-01-13 04:12:11 +0300

    add refresh guard

--- src/core/guards/index.ts
+++ src/core/guards/index.ts
@@ -1,1 +1,4 @@
 export { AuthGuard } from './auth.guard';
+export { RefreshGuard } from './refresh.guard';
+export { CheckCreatorGuard } from './check-creator.guard';
+export { RolesGuard } from './roles.guard';
--- src/core/interfaces/common/index.ts
+++ src/core/interfaces/common/index.ts
@@ -1,3 +1,5 @@
 export { PagingOptions, PagingModel } from './paging';
 export { OrderType } from './order.type';
 export { AutocompleteModel } from './autocomplete.model';
+export { GenderType } from './gender.type';
+export { RolesType } from './roles.type';
