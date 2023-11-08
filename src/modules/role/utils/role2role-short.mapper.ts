dc4ace992ec232ee80bbbaa429148c50635feeb8
DenisRybkin <denis.rybkin.94@mail.ru>
2023-11-08 19:35:08 +0300

    lint fixs

--- src/modules/role/utils/role2role-short.mapper.ts
+++
@@ -1,8 +0,0 @@
-import { Role } from 'src/database/models/singles/Role/role.model'
-import { RoleShort } from '../dto'
-
-export const role2roleShortMapper = (role: Role): RoleShort => ({
-  id: role.id,
-  description: role.description!,
-  name: role.name
-})
